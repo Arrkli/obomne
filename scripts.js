d25f0c21593a26d2a86db988bfa1320b67f23502
Arrkli <nikita.erasov2008@gmail.com>
2025-07-24 20:02:00 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -2,14 +2,9 @@
         const name = prompt("Як тебе звати?");
         
         const age = prompt("Скільки тобі років?");
-        
         if (name && age) {
-            if (parseInt(age) >= 18) {
-                alert("Привіт, " + name + "! Ти повнолітній(я). Ласкаво просимо на сторінку!");
-            } else {
-                alert("Привіт, " + name + "! Ти ще неповнолітній(я). \ Ухади з цієї сторінки!");
-                window.location.href = "https://www.google.com";
-            }
-        } else {
-            alert("Будь ласка, введи своє ім’я та вік!");
-        }
+            alert(`Привіт, ${name}! Тобі ${age} років.`);
+        }   
+        else {
+            alert("Будь ласка, введіть своє ім'я та вік.");
+        }   
